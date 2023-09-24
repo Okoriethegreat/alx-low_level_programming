686245033acb9d67dfe665687793d0cfa46cbcf4
okoriethegreat <okoriethegreat@gmail.com>
2023-09-24 14:33:52 -0700

    update 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,8 @@
-int_list - prints all the elements of a linked list
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
